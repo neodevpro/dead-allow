b69f28bd045fe02c9fe7c954663995bead2d0ed8
Fusion <leemenhin@gmail.com>
2020-08-10 03:34:09 +0800

    Add files via upload

---
+++ checkallow.py
@@ -0,0 +1,22 @@
+
+from PyFunceble import test as PyFunceble
+
+readfile = open("allow", "r")
+DOMAINS = []
+for line in readfile:
+  line_list = line.strip()
+  DOMAINS.append(line_list)
+readfile.close()
+
+
+def print_result(subject, status):
+
+    if status == "ACTIVE":
+        print(f"{domain} is {status}")
+    else:
+        file = open('deadallow', 'a')
+        file.writelines(f"{domain}\n")
+        file.close()
+
+for domain in DOMAINS:
+    print_result(domain, PyFunceble (domain))
