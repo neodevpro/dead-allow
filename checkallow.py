1298208f332d085769e1e287fea89751314e60da
Fusion <leemenhin@gmail.com>
2020-08-10 10:35:25 +0800

    Update checkallow.py

--- checkallow.py
+++ checkallow.py
@@ -11,11 +11,11 @@ readfile.close()
 def print_result(subject, status):
 
     if status == "ACTIVE":
-        print(f"{domain} is {status}")
-    else:
-        file = open('deadallow', 'a')
+        file = open('checkallow', 'a')
         file.writelines(f"{domain}\n")
         file.close()
+    else:
+        print(f"{domain} is {status}")
 
 for domain in DOMAINS:
     print_result(domain, PyFunceble (domain))
