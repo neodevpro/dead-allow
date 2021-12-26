5c85532dd89954e0a4181e74c6a56b4de866df0a
Fusion <leemenhin@gmail.com>
2021-12-26 17:13:00 +0800

    Update checkallow.py

--- checkallow.py
+++ checkallow.py
@@ -8,6 +8,7 @@ o = open("deadallow", "a")
 lines = f.readlines()
 for line in lines:
     status = checker.set_subject(line).get_status()
+    print(f"Checking {line} ?")
     if status.is_inactive() :
        o.write(f"{line}")
 f.close()
